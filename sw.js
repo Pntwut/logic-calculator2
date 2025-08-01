86bb2930ee455a900e90aa34c13dd60667988a46
Pntwut <nutvut.sridet4@gmail.com>
2025-08-01 19:37:34 +0700

    Update sw.js

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'logic-calc-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './styles.css',
+  './script.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
