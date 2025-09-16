d83620054bc4f26c2003332319430198f7294220
Pntwut <nutvut.sridet4@gmail.com>
2025-09-16 13:39:48 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,42 +1,23 @@
-const CACHE_STATIC = 'lc-static-v1.0.0';
-
-const PRECACHE_URLS = [
-  './','./index.html','./styles.css','./script.js','./manifest.json',
-  './icon-192.png','./icon-512.png'
-];
+const CACHE_STATIC = 'lc-static-v1.0.5';
+const PRECACHE_URLS = ['./','./index.html','./styles.css','./script.js','./pwa.js','./manifest.json'];
 
 self.addEventListener('install', (event) => {
   self.skipWaiting();
-  event.waitUntil(caches.open(CACHE_STATIC).then(c=>c.addAll(PRECACHE_URLS)));
+  event.waitUntil(caches.open(CACHE_STATIC).then(cache => cache.addAll(PRECACHE_URLS)));
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_STATIC?caches.delete(k):Promise.resolve())))
-      .then(()=>self.clients.claim())
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_STATIC && caches.delete(k)))).then(() => self.clients.claim()));
 });
 
 self.addEventListener('fetch', (event) => {
   const req = event.request;
   const accept = req.headers.get('accept') || '';
-
   if (req.mode === 'navigate' || accept.includes('text/html')) {
-    event.respondWith(
-      fetch(req)
-        .then(res => { caches.open(CACHE_STATIC).then(c=>c.put(req,res.clone())); return res; })
-        .catch(()=>caches.match(req).then(c=>c||caches.match('./index.html')))
-    );
+    event.respondWith(fetch(req).catch(() => caches.match('./index.html')));
     return;
   }
-
-  event.respondWith(
-    caches.match(req).then(cacheRes=>{
-      const fetchPromise = fetch(req).then(networkRes=>{
-        caches.open(CACHE_STATIC).then(c=>c.put(req, networkRes.clone()));
-        return networkRes;
-      }).catch(()=>cacheRes);
-      return cacheRes || fetchPromise;
-    })
-  );
+  event.respondWith(caches.match(req).then(res => res || fetch(req).then(net => {
+    caches.open(CACHE_STATIC).then(c => c.put(req, net.clone())); return net;
+  })));
 });
